44b802027142f09c2e63c9aade6b239a0afba003
fanyang1988 <fanyang.coder@gmail.com>
2021-05-11 18:18:27 +0800

    add cross-calling by dep package

--- contracts/cross-calling/call-by-dep/contracts/lib.rs
+++ contracts/cross-calling/call-by-dep/contracts/lib.rs
@@ -4,20 +4,28 @@ use ink_lang as ink;
 
 #[ink::contract]
 mod erc20swap {
+    use ink_env::call::FromAccountId;
+    // can add `pub use self::erc20::Erc20` to avoid this
+    use erc20::erc20::Erc20 as Erc20;
+
     #[ink(storage)]
     pub struct ERC20Swap {
         value: bool,
+        token_account_id: AccountId,
     }
 
     impl ERC20Swap {
         #[ink(constructor)]
-        pub fn new(init_value: bool) -> Self {
-            Self { value: init_value }
+        pub fn new(init_value: bool, account_id: AccountId) -> Self {
+            Self { 
+                value: init_value,
+                token_account_id: account_id,
+            }
         }
 
         #[ink(constructor)]
         pub fn default() -> Self {
-            Self::new(Default::default())
+            Self::new(Default::default(), AccountId::default())
         }
 
         #[ink(message)]
@@ -26,8 +34,14 @@ mod erc20swap {
         }
 
         #[ink(message)]
-        pub fn get(&self) -> bool {
-            self.value
+        pub fn get(&self) -> AccountId {
+            self.token_account_id
+        }
+
+        #[ink(message)]
+        pub fn balance_of(&self, owner: AccountId) -> Balance {
+            let token = <Erc20>::from_account_id(self.token_account_id);
+            return token.balance_of(owner);
         }
     }
 
@@ -44,7 +58,7 @@ mod erc20swap {
 
         #[ink::test]
         fn it_works() {
-            let mut swapper = ERC20Swap::new(false);
+            let mut swapper = ERC20Swap::new(false, AccountId::default());
             assert_eq!(swapper.get(), false);
             swapper.flip();
             assert_eq!(swapper.get(), true);
--- contracts/cross-calling/call-by-dep/tests/erc20swap.test.ts
+++ contracts/cross-calling/call-by-dep/tests/erc20swap.test.ts
@@ -6,9 +6,9 @@ const { getContractFactory, getRandomSigner } = patract;
 
 const { api, getSigners } = network;
 
-async function deployConstract(contract_path, sender){
+async function deployERC20Constract(contract_path, sender){
   const contractFactory = await getContractFactory(contract_path, sender);
-  const contract = await contractFactory.deploy('new', '1000');
+  const contract = await contractFactory.deploy('new', '1000000');
   const abi = artifacts.readArtifact(contract_path);
 
   return { contract, abi };
@@ -26,18 +26,27 @@ describe('ERC20Swap', () => {
     const sender = await getRandomSigner(Alice, one.muln(10000));
 
     // erc20
-    const { contract:contractErc20, abi:abiErc20 } = await deployConstract('erc20', sender);
+    const { contract:contractErc20, abi:abiErc20 } = await deployERC20Constract('erc20', sender);
     console.log('erc20swap ', contractErc20.address.toString(), ' ', abiErc20)
 
     // erc20swap
-    const { contract:contractSwap, abi:abiSwap } = await deployConstract('erc20swap', sender);
-    console.log('erc20swap ', contractSwap.address.toString(), ' ', abiSwap)
+    const contractFactory = await getContractFactory('erc20swap', sender);
+    const contract = await contractFactory.deploy('new', 'true', contractErc20.address.toString());
+    const abi = artifacts.readArtifact('erc20swap');
 
     const receiver = await getRandomSigner();
-    return { sender, contractSwap, abiSwap, receiver, Alice, one };
+    return { sender, contract, abi, contractErc20, receiver, Alice, one };
   }
 
   it('initial', async () => {
-    await setup();
+    const {sender, contract, abi, contractErc20, receiver, Alice, one} = await setup();
+    const result = await contract.query.get();
+    expect(result.output).to.equal(contractErc20.address);
+
+    const resultBalance = await contractErc20.query.balanceOf(sender.address);
+    expect(resultBalance.output).to.equal(1000000);
+
+    const resultBalanceProxy = await contract.query.balanceOf(sender.address);
+    expect(resultBalanceProxy.output).to.equal(1000000);
   });
 });
